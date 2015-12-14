73d5a42b9c58839014be337013704f46d2a1c127
fierlion <fierlion@gmail.com>
2015-12-14 15:23:16 -0800

    Initial Commit

---
+++ ObjectTemplate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObjectTemplate
+//
+//  Created by Ray Foote on 12/14/15.
+//  Copyright © 2015 fierlion. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
