6fb41a231955a8bcf5b47313f9ca63412dcca59b
fierlion <fierlion@gmail.com>
2015-12-14 16:01:42 -0800

    without template

--- ObjectTemplate/main.swift
+++ ObjectTemplate/main.swift
@@ -8,5 +8,22 @@
 
 import Foundation
 
-print("Hello, World!")
+var products = [
+    ("Kayak", "a boat for one person", 275.0, 4),
+    ("Lifejacket", "protective gear", 48.95, 14),
+    ("Soccer Ball", "fifa approved size and weight", 19.5, 32)
+]
+
+func calculateTax(product:(String, String, Double, Int)) -> Double {
+    return product.2 * 0.2
+}
+
+func calculateStockValue (tuples:[(String, String, Double, Int)]) -> Double {
+    return tuples.reduce(0, combine: {(total, product) -> Double in
+        return total + (product.2 * Double(product.3))
+    })
+}
+
+print("sales tax for kayak: $\(calculateTax(products[0]))")
+print("total value of stock: $\(calculateStockValue(products))")
 
